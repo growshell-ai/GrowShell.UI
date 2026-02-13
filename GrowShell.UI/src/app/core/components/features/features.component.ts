ea96adf41501f96edfbb6fdad018c667ca86f522
[vamsi98ya] <[vamsi98ya@gmail.com]>
2026-02-13 21:52:16 +0530

    Landing page changes

--- GrowShell.UI/src/app/core/components/features/features.component.ts
+++ GrowShell.UI/src/app/core/components/features/features.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, ElementRef, inject, OnDestroy, PLATFORM_ID, QueryList } from '@angular/core';
+import { Component, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
 export class FeaturesComponent {
-
+ 
 }
